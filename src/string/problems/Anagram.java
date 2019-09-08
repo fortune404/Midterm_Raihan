82de7733957f7f9470775855a6146df99e2219aa
fortune404 <rk815.rahi@gmail.com>
2019-09-08 12:24:07 -0700

    anagram

--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -1,5 +1,7 @@
 package string.problems;
 
+import java.util.Arrays;
+
 /**
  * Created by mrahman on 04/22/17.
  */
@@ -8,5 +10,26 @@ public class Anagram {
     public static void main(String[] args) {
         //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
         //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
+
+        String b = "MARY";
+        String e = "ARMY";
+        System.out.println(isAnagram(b, e));
     }
+
+
+    public static boolean isAnagram(String str1, String str2)//
+    {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        char[] c1 = str1.toCharArray();
+        Arrays.sort(c1);
+
+        char[] c2 = str2.toCharArray();
+        Arrays.sort(c2);
+
+        return Arrays.equals(c1, c2);
+    }
+
 }
